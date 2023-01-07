9c217ef70ef2f2976fbe04fb48c054a008589ccf
xcr <931025493@qq.com>
2023-01-08 01:30:03 +0800

    1/8-1:29

---
+++ 2D/Assets/Script/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform tf;
+    public Transform playertransform;
+    public double limit_l, limit_r, limit_g, limit_b;
+    public float someground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf.position = new Vector3(playertransform.position.x, 1.2f, tf.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraFollow();
+    }
+    void CameraFollow() {
+        if (tf.position.x >= limit_l && tf.position.x <= limit_r && playertransform.position.y >= limit_b - someground && playertransform.position.y <= limit_g - someground)
+            tf.position = new Vector3(playertransform.position.x, playertransform.position.y + someground, tf.position.z);
+        else if (playertransform.position.x >= limit_l && playertransform.position.x <= limit_r)
+            tf.position = new Vector3(playertransform.position.x, tf.position.y, tf.position.z);
+        else if (playertransform.position.y >= limit_b - someground && playertransform.position.y <= limit_g - someground)
+            tf.position = new Vector3(tf.position.x, playertransform.position.y + someground, tf.position.z);
+    }
+}
---
+++ 2D/Assets/Script/GhostController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+    //basic attributes of Player
+    public Transform tf;
+    public Rigidbody2D rb;
+    //speed for horizontal move and jump
+    public float speed=5f;
+    //grounded check
+    public bool istouchground;
+    public Transform[] GroundChecks = new Transform[3];
+    public LayerMask GroundLayer;
+    //animation
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        tf = GetComponent<Transform>();
+        istouchground = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove();
+        Jump();
+        AirStateChange();
+    }
+    void HorizontalMove() 
+    {
+        float direction = Input.GetAxis("Horizontal");
+        if (direction < 0)
+        {
+            tf.eulerAngles = new Vector3(tf.rotation.x, 180, tf.rotation.z);
+        }
+        else if (direction > 0)
+        {
+            tf.eulerAngles = new Vector3(tf.rotation.x, 0, tf.rotation.z);
+        }
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+    }
+    void Jump() 
+    {
+        CheckOnGround();
+        if (istouchground && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed * 1.2f);
+            AirStateChange();
+        }
+    }
+    void CheckOnGround() {
+        for (int i = 0; i < 3; i++) {
+            istouchground = Physics2D.Linecast(transform.position, GroundChecks[i].position, GroundLayer);
+            if (istouchground) return;
+        }
+    }
+    void AirStateChange()
+    {
+        if (istouchground)
+        {
+            anim.SetBool("falltofloat", true);
+            anim.SetBool("floattojump", false);
+            anim.SetBool("jumptofall", false);
+            anim.SetBool("floattofall", false);
+        }
+        else if (rb.velocity.y > 0)
+        {
+            anim.SetBool("floattojump", true);
+            anim.SetBool("falltofloat", false);
+            anim.SetBool("jumptofall", false);
+            anim.SetBool("floattofall", false);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            anim.SetBool("jumptofall", true);
+            anim.SetBool("floattofall", true);
+            anim.SetBool("falltofloat", false);
+            anim.SetBool("floattojump", false);
+        }
+    }
+}
